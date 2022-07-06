537df58594be1af739e0c5fca465660023ddc5ab
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-07-05 22:59:59 -0500

    animation listener and animation status

--- lib/src/pages/animations_page.dart
+++ lib/src/pages/animations_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class AnimationsPage extends StatelessWidget {
   const AnimationsPage({Key? key}) : super(key: key);
@@ -31,7 +32,14 @@ class _SquareAnimationState extends State<SquareAnimation>
   void initState() {
     animationController = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 4000));
-    rotation = Tween(begin: 0.0, end: 2.0).animate(animationController);
+    rotation = Tween(begin: 0.0, end: 2 * Math.pi).animate(animationController);
+
+    animationController.addListener(() {
+      print('Status: ${animationController.status}');
+      if (animationController.status == AnimationStatus.completed) {
+        animationController.reverse();
+      }
+    });
     super.initState();
   }
 
